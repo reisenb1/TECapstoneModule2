a1cb5bcc3e5b473df612a2064fea6b8d7fe35acf
Eric Reisenbuckler <eric.reisenbuckler@gmail.com>
2022-11-02 23:07:16 -0400

    Code as of 11:06 PM Wednesday

--- tenmo-client/src/main/java/com/techelevator/tenmo/App.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/App.java
@@ -4,6 +4,7 @@ import com.techelevator.tenmo.model.AuthenticatedUser;
 import com.techelevator.tenmo.model.UserCredentials;
 import com.techelevator.tenmo.services.AuthenticationService;
 import com.techelevator.tenmo.services.ConsoleService;
+import com.techelevator.tenmo.services.TenmoService;
 
 public class App {
 
@@ -11,6 +12,7 @@ public class App {
 
     private final ConsoleService consoleService = new ConsoleService();
     private final AuthenticationService authenticationService = new AuthenticationService(API_BASE_URL);
+    private final TenmoService tenmoService = new TenmoService();
 
     private AuthenticatedUser currentUser;
 
@@ -86,7 +88,8 @@ public class App {
 
 	private void viewCurrentBalance() {
 		// TODO Auto-generated method stub
-		
+        String message = String.format("Your current account balance is: $%5.2f", tenmoService.getBalance());
+        System.out.println(message);
 	}
 
 	private void viewTransferHistory() {
---
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/TenmoService.java
@@ -0,0 +1,39 @@
+package com.techelevator.tenmo.services;
+
+import com.techelevator.tenmo.model.Account;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class TenmoService {
+
+    private static final String API_BASE_URL = "http://localhost:8080/";
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private String authToken = null;
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public BigDecimal getBalance() {
+        String url = String.format("%sme/account", API_BASE_URL);
+
+        ResponseEntity<Account> response = restTemplate.exchange(url, HttpMethod.GET,
+                makeAuthEntity(), Account.class);
+        Account account = response.getBody();
+
+        return account.getBalance();
+    }
+
+    protected HttpEntity<Void> makeAuthEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(authToken);
+        return new HttpEntity<>(headers);
+    }
+}
